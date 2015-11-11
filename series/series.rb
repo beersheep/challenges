06cde16305de191ec02b4d452c476a6b42142024
beersheep <beersheep0109@gmail.com>
2015-11-12 00:24:14 +0800

    finished series challenge

---
+++ series/series.rb
@@ -0,0 +1,25 @@
+class Series
+  
+  def initialize(series)
+    @series = series
+  end
+
+  def slices(num)
+    if @series.size < num
+      raise ArgumentError.new("The series number must be longer than the piece of slices!")
+    end
+
+    result = []
+    spliting = @series.split('')
+    spliting.map! {|s| s.to_i}
+    
+    loop do 
+      break if spliting.take(num).size < num
+      result.push(spliting.take(num))
+      spliting.shift
+      break if spliting.size == 0 
+    end
+    result
+  end
+
+end
